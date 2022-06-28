df7228169de8d4b9a696cae2386318130f7cc59f
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2022-06-28 08:07:18 +0300

    Добавьте файлы проекта.

---
+++ ITAcadem.CSCourse.Lab1.ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Str;
+
+            while ((Str = Console.ReadLine()) != null)
+            {
+
+                Str = Str.Replace(",", " y:");
+                Str = "x:" + Str;
+
+                Console.WriteLine(Str);
+            }
+
+        }
+    }
+}
